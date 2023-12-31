22abdee050f87bde39a952959c0b079e0b4309ac
Mustafa Binalhag <mbin828@gmail.com>
2024-01-01 00:20:57 +0300

    domain updated to render

--- src/app/hard/utils.ts
+++ src/app/hard/utils.ts
@@ -55,5 +55,5 @@ export type ResultType = {
   currentTurn: Player
 }
 export type roomTypes = "normal" | "hard"
-export const API_URL = "192.168.8.141:3030"
+export const API_URL = "ttt-socket-server.onrender.com"
 export { createInitialBoard, createInitialBoardStats, checkDraw, convertMapToArray, checkWinner }
